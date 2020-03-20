674a11ff1c63f37693f34d07cc072475295514ae
para0056 <nick@nparadis.ca>
2020-03-19 20:49:18 -0400

    base vnet w subnets

--- network/main.tf
+++ network/main.tf
@@ -1,4 +1,22 @@
+# Create Resource Group to contain network resources
 resource "azurerm_resource_group" "main" {
-  name     = "networking-msdn-rg"
+  name     = "network_rg_name"
   location = var.location
+}
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "example"{
+  name    = var.vnet_name
+  location = var.location
+  resource_group_name = var.vnet_rg_name
+  address_space = var.cidr_bock
+}
+
+resource "azurerm_subnet" "main" {
+  count = length(var.subnet_names)
+
+  name = element(var.subnet_names, count.index)
+  resource_group_name = var.vnet_rg_name
+  virtual_network_name = var.vnet_name
+  address_prefix = element(var.subnet_cidrs, count.index)
 }
--- network/vars.tf
+++ network/vars.tf
@@ -2,3 +2,23 @@ variable "location" {
     type = string
     default = "canadacentral"
 }
+
+variable "vnet_rg_name" {
+    type= string
+}
+
+variable "vnet_name" {
+    type = string
+}
+
+variable "cidr_block" {
+    type = list(string)
+}
+
+variable "subnet_names" {
+    type = list(string)
+}
+
+variable "subnet_cidrs" {
+    type = list(string)
+}
