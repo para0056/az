eb3ebb2ac2578f9969b25fb0a4270a2080fa20dd
Nick Paradis <37662026+para0056@users.noreply.github.com>
2023-04-11 14:24:29 +0000

    add network def for vdi

--- network/main.tf
+++ network/main.tf
@@ -1,22 +1,43 @@
-# Create Resource Group to contain network resources
-resource "azurerm_resource_group" "main" {
-  name     = "network_rg_name"
-  location = var.location
+resource "azurerm_resource_group" "netrg" {
+    name     = "rg-net"
+    location = var.deploy_location
 }
 
-# Create Virtual Network
-resource "azurerm_virtual_network" "example"{
-  name    = var.vnet_name
-  location = var.location
-  resource_group_name = var.vnet_rg_name
-  address_space = var.cidr_bock
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${var.prefix}"
+  address_space       = var.vnet_range
+  location            = var.deploy_location
+  resource_group_name = azurerm_resource_group.netrg.name
+  depends_on          = [azurerm_resource_group.netrg]
 }
 
-resource "azurerm_subnet" "main" {
-  count = length(var.subnet_names)
+resource "azurerm_subnet" "vdi" {
+  name                 = "snet-vdi"
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.subnet_range
+  depends_on           = [azurerm_resource_group.netrg]
+}
 
-  name = element(var.subnet_names, count.index)
-  resource_group_name = var.vnet_rg_name
-  virtual_network_name = var.vnet_name
-  address_prefix = element(var.subnet_cidrs, count.index)
+resource "azurerm_network_security_group" "vdi" {
+  name                = "nsg-${var.prefix}"
+  location            = var.deploy_location
+  resource_group_name = azurerm_resource_group.netrg.name
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  depends_on = [azurerm_resource_group.netrg]
+}
+
+resource "azurerm_subnet_network_security_group_association" "vdi" {
+  subnet_id                 = azurerm_subnet.vdi.id
+  network_security_group_id = azurerm_network_security_group.vdi.id
 }
